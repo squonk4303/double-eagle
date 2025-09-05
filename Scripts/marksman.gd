83071bf22b874452499501b5c8c0cde25391d18d
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-05 17:51:32 +0200

    Figure out signals for firing the gun
    This also adds mock bullet tracers as visualization.

--- Scripts/marksman.gd
+++ Scripts/marksman.gd
@@ -1,14 +1,16 @@
 extends Node3D
 
-@onready var camera = $Camera3D
+@onready var pivot = $Pivot
+@onready var camera = $Pivot/Camera3D
 
-@export var bullet_scene: PackedScene
 @export var mouse_sens = 0.02
 
 var MAX_PITCH = deg_to_rad(90)
 var MIN_PITCH = deg_to_rad(-90)
 
 var to_rotate: Vector3
+
+signal gun0_fired(b_position: Vector3, b_rotation: Vector3)
 
 func _ready():
     # Capture mouse
@@ -25,12 +27,10 @@ func _unhandled_input(event):
         to_rotate.x = -1 * event.relative.y * mouse_sens
 
     if event.is_action_pressed("primary_fire"):
-        print("Firing @ ", camera.rotation)
-        var bullet = bullet_scene.instantiate()
-        var bullet_position = camera.position + Vector3(1,1,1)
-        bullet.initialize(camera.position, camera.rotation)
-
-        add_child(bullet)
+        # Tweak position before emitting
+        var bullet_position = camera.global_position + Vector3(0.1, -0.1, -0.1)
+        # Emit signal to spawn a bullet in parent scene
+        gun0_fired.emit(bullet_position, camera.global_rotation)
 
     if event.is_action_pressed("LMB"):
         # Recapture mouse upon clicking inside window
@@ -46,7 +46,7 @@ func _process(delta: float) -> void:
     # Find this out.
 
     # Rotate Pivot along y-axis
-    rotate_y(to_rotate.y * delta)
+    pivot.rotate_y(to_rotate.y * delta)
     # Rotate Camera along its x-axis
     camera.rotate_x(to_rotate.x * delta)
     camera.rotation.x = clamp(camera.rotation.x, MIN_PITCH, MAX_PITCH)
--- Scripts/shooting_gallery.gd
+++ Scripts/shooting_gallery.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var ball_scene: PackedScene
+@export var bullet_scene: PackedScene
 
 
 func _on_ball_timer_timeout() -> void:
@@ -11,3 +12,11 @@ func _on_ball_timer_timeout() -> void:
 
     # Spawn by adding the object to the main scene
     add_child(ball)
+
+func _on_marksman_gun_0_fired(b_position: Vector3, b_rotation: Vector3) -> void:
+    pass # Replace with function body.
+
+    print("Firing @ ", b_position, b_rotation)
+    var bullet = bullet_scene.instantiate()
+    bullet.initialize(b_position, b_rotation)
+    add_child(bullet)
