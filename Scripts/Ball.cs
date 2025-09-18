01f9dfcbacc9dc2ca9de462debdba0aa7f1e3900
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-18 12:15:56 +0200

    Sort of restore Ball.cs
    Still style to change, when the time comes.

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -8,4 +8,21 @@ public partial class Ball : RigidBody3D
         this.Position = startPosition;
         ApplyForce(new Vector3(1, 1, 0) * 350);
     }
+
+    public void bullet_hit(Vector3 b_pos)
+    {
+        // Get direction from center of bullet to center of self
+        Vector3 direction = b_pos.DirectionTo(GlobalPosition);
+        // Suppress force applied in z-direction
+        // NOTE: This denormalizes the vector
+        direction.Z = 0.0F;
+        ApplyForce(direction * 1200);
+    }
+
+    public void _on_visible_on_screen_notifier_3d_screen_exited()
+    {
+        // TODO: Will despawn anytime player looks away, which is unintended
+        GD.Print("freeing ", this);
+        QueueFree();
+    }
 }
