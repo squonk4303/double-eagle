9fa7852eabc9e6e916292e8de490a4b1c0bf6f4a
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-18 10:31:58 +0200

    Small update

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,15 +1,17 @@
 extends Node3D
 
 const BULLET_SPEED = 140
-
-const KILL_TIMER = 4
-var timer = 0
+const KILL_TIMER = 4.0
 
 var hit_something_yet = false
 
 
 func _ready():
     $Area3D.body_entered.connect(self.collided)
+
+    # Start timer after which bullet despawns
+    await get_tree().create_timer(KILL_TIMER).timeout
+    queue_free()
 
 
 func initialize(start_position: Vector3, start_rotation: Vector3):
