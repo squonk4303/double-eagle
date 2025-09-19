5c58ec9c41c11be41379063e1d86c605a7d069a9
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-20 00:57:41 +0200

    Squashed commit which improves ball spawning
    commit 13c6210b082ccabc0790a6c257484620bb94aca6
    Author: Wilhelmsen <wwilhelmsen@sogn.no>
    Date:   Sat Sep 20 00:46:19 2025 +0200
    
        Finalize improved ball spawning
    
        I found a much better method for getting balls to come from both sides
        of the screen. First method was to use two paths, but that caused
        problems with choosing one at random (I could've got a jank solution,
        and may *have* to in the future).
    
        The better solution was to transform the spawn location right before
        spawning. This should also save on performance, since we only need 1
        node object. If we need one which isn't near (0,0,0), though,  we'll
        have to apply an offset, too. Also I'm satisfied with the
        {0, 1} -> {-1, 1} function.
    
        Closes: #7
    
    commit 5c9eaf57cc28b48e51e23c028fc6fd750de3fc1f
    Author: Wilhelmsen <wwilhelmsen@sogn.no>
    Date:   Fri Sep 19 23:38:32 2025 +0200
    
        Improve ball spawn-force formula
    
        Updates: #7
    
    commit cd4af547e245b139eb4740806e78a68b58c860ba
    Author: Wilhelmsen <wwilhelmsen@sogn.no>
    Date:   Fri Sep 19 23:21:25 2025 +0200
    
        Make random spawning sorta work
    
        Updates: #7
    
    commit c12d465cfb8ae4e60c1eb7078ddfa38bcbea33a4
    Author: Wilhelmsen <wwilhelmsen@sogn.no>
    Date:   Fri Sep 19 23:20:22 2025 +0200
    
        Add spawn locations for random spawning
    
        Updates: #7

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -3,13 +3,20 @@ using System;
 
 public partial class Ball : RigidBody3D
 {
-    private const float FORCEFULNESS = 1200.0f;
+    private const float COLLISION_FORCE = 1200.0f;
+    private const float ENTRY_FORCE = 60.0f;
 
     /// Prepare for getting spawned in
-    public void Initialize(Vector3 startPosition)
+    public void Initialize(Vector3 spawn, Vector3 target)
     {
-        this.Position = startPosition;
-        ApplyForce(new Vector3(1.0f, 1.0f, 0.0f) * 350.0f);
+        GD.Print(DateTime.Now.Second);
+        Position = spawn;
+        Vector3 difference = target - spawn;
+        difference.Z = 0.0f;
+        difference *= 0.5f;
+        difference.Y += 3.0f;
+        ApplyForce(difference * (ENTRY_FORCE + GD.Randf() * 40.0f));
+        GD.Print(difference);
     }
 
     /// How to respond when hit by a bullet
@@ -20,7 +27,7 @@ public partial class Ball : RigidBody3D
         // Suppress force applied in z-direction
         // NOTE: This denormalizes the vector
         direction.Z = 0.0f;
-        ApplyForce(direction * FORCEFULNESS);
+        ApplyForce(direction * COLLISION_FORCE);
     }
 
     /// What to do when leaving the player's FOV
