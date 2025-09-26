ded7b4b38a3f061a0889183a160e87faca07cfac
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-27 01:22:53 +0200

    Progress on laser penetration
    Updates: #10

--- Scripts/Laser.cs
+++ Scripts/Laser.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Laser : Node3D
 {
+    private const int MAX_PENETRATIONS = 4;
+
     private AnimationPlayer _animation;
     private Boolean _doneRaycast = false;
 
@@ -22,43 +24,54 @@ public partial class Laser : Node3D
 
     public override void _PhysicsProcess(double delta)
     {
-        if (!_doneRaycast)
+        // Check raycast collision only on first frame
+
+        // Check for raycast collision
+        var ray = GetNode<RayCast3D>("RayCast3D");
+        ray.CollideWithAreas = true;
+        ray.Enabled = true;
+
+        // Objects the laser will ignore due to it penetrating through them.
+        var exceptions = new Godot.Collections.Array<CollisionObject3D>();
+
+        Boolean keepChecking = true;
+        int penetrationsLeft = MAX_PENETRATIONS;
+
+        while (penetrationsLeft >= 0 && keepChecking)
         {
-            // ---------------------------------------------------------------------------
-            // Check for raycast collision
-            var ray = GetNode<RayCast3D>("RayCast3D");
-            ray.ForceRaycastUpdate();
-            ray.CollideWithAreas = true;
-            ray.Enabled = true;
-
-            Boolean going = true;
-            int counter = 0;
-
-            while (counter <= 5 && going)
+            // Don't check again for objects which have been penetrated through
+            foreach (var e in exceptions)
             {
-                GD.Print("Loopin " + counter);
-                if (ray.IsColliding())
-                {
-                    var collider = ray.GetCollider() as CollisionObject3D;
-                    GD.Print("Collided with ", collider);
-                    if (collider.HasMethod("LaserHit"))
-                    {
-                        collider.Call("LaserHit");
-                    }
-                    ray.AddException(collider);
-                    // Add collider to list of exceptions
-                    // And add list of exceptions to ray exceptions
-                }
-                else
-                {
-                    going = false;
-                }
-
-                counter += 1;
+                ray.AddException(e);
             }
-            // ---------------------------------------------------------------------------
-            _doneRaycast = true;
+
+            ray.ForceRaycastUpdate();
+
+            GD.Print("Loopin " + penetrationsLeft);
+            if (ray.IsColliding())
+            {
+                var collider = ray.GetCollider() as CollisionObject3D;
+                GD.Print("Collided with ", collider);
+                if (collider.HasMethod("LaserHit"))
+                {
+                    collider.Call("LaserHit");
+                }
+                exceptions.Add(collider);
+                // Add collider to list of exceptions
+                // And add list of exceptions to ray exceptions
+            }
+            else
+            {
+                // Stop checking for hits after hitting a wall or null
+                keepChecking = false;
+            }
+
+            penetrationsLeft -= 1;
         }
+        // ---------------------------------------------------------------------------
+
+        // Remove Raycast from scene so as to only check collisions for the first frame
+        ray.QueueFree();
     }
 
     private void OnAnimationFinished(String animName)
