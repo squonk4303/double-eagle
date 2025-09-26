07f51de0dcb5a5407d3978303afaa069240a18f1
Wilhelmsen <wwilhelmsen@sogn.no>
2025-09-27 01:52:42 +0200

    Implement laser penetration
    A bit harder than I expected. This also changes the laser to only have
    one (1) active frame for collisions, but in that one frame, it will
    check for any number of collisions in a row, up to the raycast length.
    
    Updates: #10

--- Scripts/Laser.cs
+++ Scripts/Laser.cs
@@ -3,10 +3,9 @@ using System;
 
 public partial class Laser : Node3D
 {
-    private const int MAX_PENETRATIONS = 4;
+    private const int MAX_COLLISIONS = 2;
 
     private AnimationPlayer _animation;
-    private Boolean _doneRaycast = false;
 
     public override void _Ready()
     {
@@ -24,51 +23,56 @@ public partial class Laser : Node3D
 
     public override void _PhysicsProcess(double delta)
     {
-        // Check raycast collision only on first frame
+        CheckPenetrations();
+    }
 
-        // Check for raycast collision
+    /// Check for ray collisions, allowing for multiple, through penetration
+    private void CheckPenetrations()
+    {
+        // Retrieve the Raycast object and do some alterations
         var ray = GetNode<RayCast3D>("RayCast3D");
         ray.CollideWithAreas = true;
         ray.Enabled = true;
 
-        // Objects the laser will ignore due to it penetrating through them.
+        // Track objects the laser will ignore due to it penetrating through them.
         var exceptions = new Godot.Collections.Array<CollisionObject3D>();
 
+        // Do a new raycast collision check after each penetration left
         Boolean keepChecking = true;
-        int penetrationsLeft = MAX_PENETRATIONS;
+        int collisionsLeft = MAX_COLLISIONS;
 
-        while (penetrationsLeft >= 0 && keepChecking)
+        while (collisionsLeft > 0 && keepChecking)
         {
-            // Don't check again for objects which have been penetrated through
+            // Ignore collision with objects through which have already been penetrated
             foreach (var e in exceptions)
             {
                 ray.AddException(e);
             }
 
+            // Update raycast before end of physics frame.
+            // This ensures the multiple collisions are enacted
+            // without the need for multiple active frames.
             ray.ForceRaycastUpdate();
 
-            GD.Print("Loopin " + penetrationsLeft);
             if (ray.IsColliding())
             {
                 var collider = ray.GetCollider() as CollisionObject3D;
-                GD.Print("Collided with ", collider);
+                // Prompt collision subject for their reaction
                 if (collider.HasMethod("LaserHit"))
                 {
                     collider.Call("LaserHit");
                 }
+                // Ignore collision subject in subsequent collision checks
                 exceptions.Add(collider);
-                // Add collider to list of exceptions
-                // And add list of exceptions to ray exceptions
             }
             else
             {
-                // Stop checking for hits after hitting a wall or null
+                // Stop checking for collisions after hitting a wall or null
                 keepChecking = false;
             }
 
-            penetrationsLeft -= 1;
+            collisionsLeft -= 1;
         }
-        // ---------------------------------------------------------------------------
 
         // Remove Raycast from scene so as to only check collisions for the first frame
         ray.QueueFree();
