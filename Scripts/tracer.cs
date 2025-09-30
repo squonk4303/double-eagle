3644aad5cb23ff1be8d261055f8a99878fdfabec
Adam Saether <adamen2402@gmail.com>
2025-09-30 16:26:23 +0200

    Change from tab- to 4 space indentation

--- Scripts/tracer.cs
+++ Scripts/tracer.cs
@@ -2,24 +2,21 @@ using Godot;
 
 public partial class Tracer : Node3D
 {
-	// Uncomment and assign when RayCast3D
-	// [Export]
-	// private RayCast3D ray;
-
-	public void Initialize(Vector3 startPosition, Vector3 startRotation)
-	{
-		Position = startPosition;
-		Rotation = startRotation;
-	}
-
-	public void OnBodyEntered()
-	{
-		GD.Print("something entered bullet");
-	}
-
-	public override void _PhysicsProcess(double delta)
-	{
-		// Uncomment to free the node when needed
-		// QueueFree();
-	}
+    // Uncomment and assign when RayCast3D
+    // [Export]
+    // private RayCast3D ray;
+    public void Initialize(Vector3 startPosition, Vector3 startRotation)
+    {
+        Position = startPosition;
+        Rotation = startRotation;
+    }
+    public void OnBodyEntered()
+    {
+        GD.Print("something entered bullet");
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        // Uncomment to free the node when needed
+        // QueueFree();
+    }
 }
