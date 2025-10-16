9e9855c1911dfd89d0f161c6b8718e547d6f5423
Adam Saether <adamen2402@gmail.com>
2025-10-16 16:29:51 +0200

    Make it so the game automatically captues the mouse when backing out of pause menu; you now only have to capture the mouse once, on startup

--- Scripts/PauseMenu.cs
+++ Scripts/PauseMenu.cs
@@ -15,6 +15,7 @@ public partial class PauseMenu : Control
         GetNode<Button>("PauseButtons/MainMenuButton").Pressed += OnMainMenuPressed;
 
         Visible = false;
+        // Ensure menu proecesses even when game is paused
         ProcessMode = ProcessModeEnum.Always;
     }
 
@@ -26,11 +27,22 @@ public partial class PauseMenu : Control
 
     public void TogglePause()
     {
-        Input.MouseMode = Input.MouseModeEnum.Visible;
+        // Toggle (in)visiblity and (un)pause
         Visible = !Visible;
-        GetTree().Paused = !GetTree().Paused;
-        pauseButtons.Visible = true;
-
+        bool isPaused = !GetTree().Paused;
+        GetTree().Paused = isPaused;
+        if (isPaused)
+        {
+            // Pausing - show cursor
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+            pauseButtons.Visible = true;
+        }
+        else
+        {
+            // Unpausing - capture mouse
+            Input.MouseMode = Input.MouseModeEnum.Captured;
+        }
+        // Clear options panel if it exists
         if (optionsContainer != null && optionsContainer.GetChildCount() > 0)
         {
             foreach (Node child in optionsContainer.GetChildren())
